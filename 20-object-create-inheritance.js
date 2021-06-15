2c2981c399f3a075b62f5db16d8d6f6b08f11109
Pratik Dhungel <dhungel.1996@gmail.com>
2021-06-15 15:22:38 +0545

    Remove comments

--- 20-object-create-inheritance.js
+++ 20-object-create-inheritance.js
@@ -1,20 +1,20 @@
-// let person = {
-//   firstName: 'Default',
-//   lastName: 'Default',
-//   greet: function () {
-//     return 'Hello ' + this.firstName;
-//   },
-// };
+let person = {
+  firstName: 'Default',
+  lastName: 'Default',
+  greet: function () {
+    return 'Hello ' + this.firstName;
+  },
+};
 
-// // This will create an empty object with the prototype set to the object passed in
-// // So the __proto__ of 'john' object will have 'person'
-// let john = Object.create(person);
+// This will create an empty object with the prototype set to the object passed in
+// So the __proto__ of 'john' object will have 'person'
+let john = Object.create(person);
 
-// // We can assign values to the new object and we can still have the greet method
-// john.firstName = 'John';
-// john.lastName = 'Doe';
-// console.log(john);
-// console.log(john.greet());
+// We can assign values to the new object and we can still have the greet method
+john.firstName = 'John';
+john.lastName = 'Doe';
+console.log(john);
+console.log(john.greet());
 
 // Extra...
 // Creating a polyfill for the Object.create method
